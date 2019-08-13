89f6df37a798a33e7ee4615b51dd891b2165941e
keerthimohan93 <keerthi_mohan14@yahoo.com>
2019-08-13 15:10:55 +0530

    deployment

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: path.resolve(__dirname, 'assets')
+    publicPath: '/'
   },
   devServer: {
     open: true,
