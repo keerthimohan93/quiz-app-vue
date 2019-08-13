ca5fa658315996f221a9dedcf72050b0208c01cb
keerthimohan93 <keerthi_mohan14@yahoo.com>
2019-08-13 14:10:36 +0530

    add publicPath in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const fs = require('fs');
+const path=require('path');
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    publicPath: './src/assets',
+    filename:path.join(__dirname+'dist/bundle.js');
+  },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
