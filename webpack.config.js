b3b1c124116bdd5d404f4b1ea23d3adfaacad0ff
keerthimohan93 <keerthi_mohan14@yahoo.com>
2019-08-13 14:15:13 +0530

    rolled back

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const fs = require('fs');
-const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    publicPath: './src/assets'
-  },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
