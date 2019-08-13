bc9b9977a9ed2e822a5524c0e29b78ee1ddd63ee
keerthimohan93 <keerthi_mohan14@yahoo.com>
2019-08-13 15:05:13 +0530

    Added file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,8 +9,16 @@ module.exports = {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] }
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
     ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     open: true,
